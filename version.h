25877dc5037ca00633b260c2262d42bffff95e81
mxr <123785508+mxrcode@users.noreply.github.com>
2023-05-01 17:10:10 +0300

    Fixed, used hex value in MAGIC

--- version.h
+++ version.h
@@ -8,6 +8,6 @@ const QString SOFT_NAME = "TaskGuard";
 const QString SOFT_VERSION = "1.1.1";
 
 // Used to encrypt various data in the application, we recommend that you change this value "QString(value)" for private use.
-const QString MAGIC = QCryptographicHash::hash(QString("fph0CSccBvmmuwfnku9SsgD0ByvyZ1nwLAM2zYrOcJt1cI&5BbVtzyVApAEyHNuP").toUtf8() + QSysInfo::machineUniqueId(), QCryptographicHash::Blake2b_512);
+const QString MAGIC = QCryptographicHash::hash(QString("s%3iSyOVLYGGjhXOY9O3DG!dx4MHHr#uPGVUsEt0e8pyQtlYS0EnTCTVcW76I6jR").toUtf8() + QSysInfo::machineUniqueId(), QCryptographicHash::Blake2b_512).toHex();
 
 #endif // VERSION_H
