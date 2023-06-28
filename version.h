a0a8b1873a09d41f89f0df45e41fa4222228b003
mxrcode <123785508+mxrcode@users.noreply.github.com>
2023-06-28 15:53:54 +0300

    Changing the current version (1.2.0)

--- version.h
+++ version.h
@@ -5,9 +5,9 @@
 #include <QCryptographicHash>
 
 const QString SOFT_NAME = "TaskGuard";
-const QString SOFT_VERSION = "1.1.1";
+const QString SOFT_VERSION = "1.2.0";
 
 // Used to encrypt various data in the application, we recommend that you change this value "QString(value)" for private use.
-const QString MAGIC = QCryptographicHash::hash(QString("s%3iSyOVLYGGjhXOY9O3DG!dx4MHHr#uPGVUsEt0e8pyQtlYS0EnTCTVcW76I6jR").toUtf8() + QSysInfo::machineUniqueId(), QCryptographicHash::Blake2b_512).toHex();
+const QString MAGIC = QCryptographicHash::hash(QString("XNN02Ot7VbVfNBix8psmIXow1zcVu5fCknu&1uPVvynfNwdkCPH4OAOUPDJQbOzbW0Ykroi8GMMiS4qhzm6N57AUJv3yeleTRuedBAert7QEqTe3RQvU2sAKX4XVXPAe").toUtf8() + QSysInfo::machineUniqueId(), QCryptographicHash::Blake2b_512).toHex();
 
 #endif // VERSION_H
